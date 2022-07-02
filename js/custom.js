5c2928f8fbacf0a5b4c0527338195679feda3b10
Jatin Shrivastava <jatins@athmin.com>
2022-07-02 21:47:32 +0530

    Worked on Charts Fixed mask issue

--- js/custom.js
+++ js/custom.js
@@ -61,17 +61,18 @@ var ctxB = document.getElementById("barChart").getContext('2d');
 var myBarChart = new Chart(ctxB, {
     type: 'bar',
     data: {
-        labels: ["C", "Python", "Java", "C++", "Obj C"],
+        labels: ["Dart", "Swift", "Kotlin", "C", "Python", "Java", "C++"],
         datasets: [{
             label: 'Skill Level',
-            data: [9, 7, 6, 8, 5],
+            data: [9,8,6,7,5,5,4],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-
+                'rgba(236, 126, 6, 0.2)',
+                'rgba(175, 137, 200, 0.2)',
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
@@ -79,7 +80,8 @@ var myBarChart = new Chart(ctxB, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-
+                'rgba(236, 126, 6, 1)',
+                'rgba(175, 137, 200, 1)',
             ],
             borderWidth: 1
         }]
@@ -94,7 +96,13 @@ var myBarChart = new Chart(ctxB, {
                     max: 10,
                     min: 0,
                     stepSize: 1,
-                    beginAtZero: true
+                    beginAtZero: true,
+                    fontColor: "#93a2c2",
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontColor: "#93a2c2",
                 }
             }]
         }
@@ -109,10 +117,10 @@ var ctxL = document.getElementById("lineChart").getContext('2d');
 var myLineChart = new Chart(ctxL, {
     type: 'line',
     data: {
-        labels: ["HTML", "CSS", "JavaScript", "Django", "Bootstrap", ],
+        labels: ["Flutter", "Django", "HTML", "CSS", "JavaScript", "PHP", "MYSQL", ],
         datasets: [{
                 label: "Skill Level",
-                data: [9, 8, 6, 8, 7],
+                data: [9, 7, 9, 8, 6, 5, 6],
                 backgroundColor: [
                     'rgba(105, 0, 132, .2)',
                 ],
@@ -135,7 +143,13 @@ var myLineChart = new Chart(ctxL, {
                     max: 10,
                     min: 0,
                     stepSize: 1,
-                    beginAtZero: true
+                    beginAtZero: true,
+                    fontColor: "#93a2c2",
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontColor: "#93a2c2",
                 }
             }]
         }
@@ -163,12 +177,131 @@ var myRadarChart = new Chart(ctxR, {
         ]
     },
     options: {
+        scale:{
+            pointLabels:{
+               fontColor:"#93a2c2",
+            },
+            ticks: {
+                display: false
+              }
+        },
         legend: {
             display: false,
         },
         responsive: true
     }
 });
+
+var radarOptions = {
+				
+	//Boolean - If we show the scale above the chart data			
+	scaleOverlay : false,
+	
+	//Boolean - If we want to override with a hard coded scale
+	scaleOverride : false,
+	
+	//** Required if scaleOverride is true **
+	//Number - The number of steps in a hard coded scale
+	scaleSteps : null,
+	//Number - The value jump in the hard coded scale
+	scaleStepWidth : null,
+	//Number - The centre starting value
+	scaleStartValue : null,
+	
+	//Boolean - Whether to show lines for each scale point
+	scaleShowLine : true,
+
+	//String - Colour of the scale line	
+	scaleLineColor : "#999",
+	
+	//Number - Pixel width of the scale line	
+	scaleLineWidth : 1,
+
+	//Boolean - Whether to show labels on the scale	
+	scaleShowLabels : false,
+	
+	//Interpolated JS string - can access value
+	scaleLabel : "<%=value%>",
+	
+	//String - Scale label font declaration for the scale label
+	scaleFontFamily : "'Arial'",
+	
+	//Number - Scale label font size in pixels	
+	scaleFontSize : 12,
+	
+	//String - Scale label font weight style	
+	scaleFontStyle : "normal",
+	
+	//String - Scale label font colour	
+	scaleFontColor : "#666",
+	
+	//Boolean - Show a backdrop to the scale label
+	scaleShowLabelBackdrop : true,
+	
+	//String - The colour of the label backdrop	
+	scaleBackdropColor : "rgba(255,255,255,0.75)",
+	
+	//Number - The backdrop padding above & below the label in pixels
+	scaleBackdropPaddingY : 2,
+	
+	//Number - The backdrop padding to the side of the label in pixels	
+	scaleBackdropPaddingX : 2,
+	
+	//Boolean - Whether we show the angle lines out of the radar
+	angleShowLineOut : true,
+	
+	//String - Colour of the angle line
+	angleLineColor : "rgba(255,255,255,0.3)",
+	
+	//Number - Pixel width of the angle line
+	angleLineWidth : 1,			
+	
+	//String - Point label font declaration
+	pointLabelFontFamily : "'Arial'",
+	
+	//String - Point label font weight
+	pointLabelFontStyle : "normal",
+	
+	//Number - Point label font size in pixels	
+	pointLabelFontSize : 12,
+	
+	//String - Point label font colour	
+	pointLabelFontColor : "#EFEFEF",
+	
+	//Boolean - Whether to show a dot for each point
+	pointDot : true,
+	
+	//Number - Radius of each point dot in pixels
+	pointDotRadius : 3,
+	
+	//Number - Pixel width of point dot stroke
+	pointDotStrokeWidth : 1,
+	
+	//Boolean - Whether to show a stroke for datasets
+	datasetStroke : true,
+	
+	//Number - Pixel width of dataset stroke
+	datasetStrokeWidth : 1,
+	
+	//Boolean - Whether to fill the dataset with a colour
+	datasetFill : true,
+	
+	//Boolean - Whether to animate the chart
+	animation : true,
+
+	//Number - Number of animation steps
+	animationSteps : 60,
+	
+	//String - Animation easing effect
+	animationEasing : "easeOutQuart",
+
+	//Function - Fires when the animation is complete
+	onAnimationComplete : null,
+  
+  //make it respond damnit!
+  responsive: true
+	
+}
 
 var docWidth = document.documentElement.offsetWidth;
 
